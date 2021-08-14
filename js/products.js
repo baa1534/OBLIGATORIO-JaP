3fcff0addc04fd2e1c131a881200c0f986e15059
baa1534 <88352174+baa1534@users.noreply.github.com>
2021-08-14 18:19:20 -0300

    Armar lista de productos con sus caracteristicas

--- js/products.js
+++ js/products.js
@@ -3,4 +3,63 @@
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
 
+
+
+// VEAMOS SI TENEMOS ALGUN PROBLEMA CON EL JSON ANTES DE PONERNOS A HACER NADA CON LOS DATOS
+
+    getJSONData(PRODUCTS_URL).then(function(datosTraidos){
+        if (datosTraidos.status === "ok")
+        {
+            datosUtiles = datosTraidos.data;
+            MostrarListaProductos(datosUtiles);
+        } else {
+          alert (datosTraidos.data) ;
+        }
+    });
+
+
+
+// SI getJSONData FUNCIONA, TRABAJO CON LOS DATOS LEVANTADOS
+
+function MostrarListaProductos(datos) {
+
+let divinteres = document.getElementsByTagName("div")[1];
+divinteres.innerHTML = "";
+
+
+let tabla = document.createElement("table");
+let cuerpotabla = document.createElement("tbody");
+cuerpotabla.id = "cuerpotabla";
+cuerpotabla.style.textAlign = "center"
+
+tabla.appendChild(cuerpotabla);
+divinteres.appendChild(tabla);
+
+datos.forEach(producto => {  
+                
+    let prodrow = "";
+
+    prodrow = `
+            <tr> 
+
+            <td rowspan="2"> <img src=` + producto.imgSrc + ` width=200px </td>
+            <td>` + producto.name + `</td>
+            <td>` + producto.currency + producto.cost + `</td>
+            <td> Vendidos:` + producto.soldCount + `</td>
+
+            </tr>
+
+            <tr> 
+
+            <td colspan="3">` + producto.description + `</td>
+            
+            </tr>
+         `
+        document.getElementById("cuerpotabla").innerHTML += prodrow;
+         
+
+            });
+
+        }
+  
 });
