de1b4095731f1bfdac72553d478ab7bdd576865f
baa1534 <88352174+baa1534@users.noreply.github.com>
2021-08-18 22:20:16 -0300

    Fake login

--- js/login.js
+++ js/login.js
@@ -3,4 +3,31 @@
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
 
+document.getElementById("AutenticationBtn").addEventListener("click", function() {
+
+    
+    let UserName = document.getElementById("InputUserName");
+    let PassWord = document.getElementById("InputPassWord");
+
+
+
+if (UserName.value === "" || PassWord.value === ""){
+    alert("Se deben completar ambos campos!");
+    
+} else {
+
+   if (UserName.value == "usuario" && PassWord.value == "usuario"){
+       alert("Autenticacion exitosa!")
+window.location = "inicio.html";
+
+    } else {
+       alert("Login de prueba! Ingresar con Usuario: usuario y Contraseña: usuario");
+   }
+}
+
+
+
+})
+
+ 
 });
