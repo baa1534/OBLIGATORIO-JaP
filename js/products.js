b9f592d9d99c5a6824e0737e77edf6069b5cd716
baa1534 <88352174+baa1534@users.noreply.github.com>
2021-08-25 16:49:48 -0300

    Darle funcionalidad al filtro

--- js/products.js
+++ js/products.js
@@ -3,7 +3,10 @@
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
 
-
+// LAS DEFINO AHORA, SIN CONTENIDO, PARA QUE NO ME DE ERROR SI EL USUARIO NO FILTRA NADA
+let PrecioMin = undefined;
+let PrecioMax = undefined;
+let Moneda = undefined;
 
 // VEAMOS SI TENEMOS ALGUN PROBLEMA CON EL JSON ANTES DE PONERNOS A HACER NADA CON LOS DATOS
 
@@ -17,11 +20,9 @@ document.addEventListener("DOMContentLoaded", function (e) {
         }
     });
 
-
-
 // SI getJSONData FUNCIONA, TRABAJO CON LOS DATOS LEVANTADOS
 
-function MostrarListaProductos(datos) {
+function MostrarListaProductos(DatosOrig) {
 
 let divinteres = document.getElementById("ContenedorLista");
 divinteres.innerHTML = "";
@@ -35,9 +36,15 @@ cuerpotabla.style.textAlign = "center"
 tabla.appendChild(cuerpotabla);
 divinteres.appendChild(tabla);
 
-datos.forEach(producto => {  
-                
-    let prodrow = "";
+let datos = DatosOrig.filter(producto => {
+
+return (PrecioMin == undefined || (PrecioMin != undefined && producto.cost >= PrecioMin)) && (PrecioMax == undefined || (PrecioMax != undefined && producto.cost <= PrecioMax));
+
+})
+
+datos.forEach(producto => {
+    
+        let prodrow = "";
 
     prodrow = `
             <tr> 
@@ -57,9 +64,79 @@ datos.forEach(producto => {
          `
         document.getElementById("cuerpotabla").innerHTML += prodrow;
          
-
+    
             });
 
         }
   
+
+// ACCIONO SEGUN EL USUARIO HAYA SELECCIONADO O NO ALGO EN LOS FILTROS
+
+// LEVANTO LOS VALORES DEL FILTRO INGRESADOS POR EL CLIENTE
+document.getElementById("InputFiltrarBtn").addEventListener("click", function() {
+
+PrecioMin = document.getElementById("InputPrecioMin").value;
+PrecioMax = document.getElementById("InputPrecioMax").value;
+Moneda = document.getElementById("InputMoneda").value;
+
+let AuxMax = 0;
+let AuxMin = 0;
+
+alert("El filtro anda");
+
+if((PrecioMax != undefined) && (PrecioMax != "") && (parseInt(PrecioMax) > 0)){ //QUE ERROR PODRIA HABER SI MI CONDICION ES UNICAMENTE QUE EL PARSEINT DEL PRECIO MINIMO SEA MAYOR A CERO?
+    PrecioMax = parseInt(PrecioMax);
+    AuxMax = 1;
+
+} else {
+    alert("elegi otro maximo");
+    PrecioMax = undefined;
+    document.getElementById("InputPrecioMax").value = ""; // QUE NO ME SIGA MOSTRANDO EL VALOR QUE ESTA MAL
+    
+}
+
+
+
+if((PrecioMin != undefined) && (PrecioMin != "") && (parseInt(PrecioMin) > 0)){ 
+    PrecioMin = parseInt(PrecioMin);
+    AuxMin = 1;
+    
+} else {
+    alert("elegi otro minimo");
+    PrecioMin = undefined;
+    document.getElementById("InputPrecioMin").value = ""; // QUE NO ME SIGA MOSTRANDO EL VALOR QUE ESTA MAL
+    
+}
+
+// SI PASA QUE EL CLIENTE INGRESA LOS VALORES DE MAXIMO Y MINIMO EN LOS INPUTS AL REVES, LOS DOY VUELTA
+if (((AuxMax === 1) && (AuxMin ===1)) && (PrecioMin >= PrecioMax)){
+    PrecioMax = parseInt(document.getElementById("InputPrecioMin").value);
+    PrecioMin = parseInt(document.getElementById("InputPrecioMax").value);
+    alert (PrecioMin);
+    alert (PrecioMax);
+    document.getElementById("InputPrecioMin").value = PrecioMin;
+    document.getElementById("InputPrecioMax").value = PrecioMax;
+}
+
+MostrarListaProductos(datosUtiles); //TIENE QUE VOLVER A CORRER CON LOS VALORES DEL FILTRO
+
+})
+
+// LIMPIO LOS VALORES DEL FILTRO
+document.getElementById("InputResetBtn").addEventListener("click", function() {
+
+    PrecioMin = undefined;
+    PrecioMax = undefined;
+    Moneda = undefined;
+
+    document.getElementById("InputPrecioMin").value = "";
+    document.getElementById("InputPrecioMax").value = "";
+    document.getElementById("InputMoneda").value = "ElijeMoneda";
+
+    alert("El reset anda");
+    
+    MostrarListaProductos(datosUtiles); //TIENE QUE VOLVER A CORRER CON LOS VALORES DEL FILTRO
+    
+    })
+
 });
